6113df62fa475a2969e5d39ff4201bdb53539bca
German Martinez <mmgerman89@gmail.com>
2014-09-04 01:28:55 -0300

    Adding tags to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,25 @@
 class Event < ActiveRecord::Base
   belongs_to :organizers, class_name: "User"
+  has_many :taggings
+  has_many :tags, through: :taggings
   extend FriendlyId
   friendly_id :slug_candidates, use: :slugged
+  
   
   def slug_candidates
     [
       :title,
       [:title, :location]
     ]
+  end
+  
+  def all_tags=(names)
+      self.tags = names.split(",").map do |t|
+        Tag.where(name: t.strip).first_or_create!
+      end
+    end
+  
+  def all_tags
+    tags.map(&:name).join(", ")
   end
 end
